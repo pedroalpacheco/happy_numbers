e391427ec71a635166897fa6f1a096a63c04eef2
papacheco <pedro.pacheco.a@gmail.com>
2017-06-01 15:32:14 -0300

    Modificação1

---
+++ happy_numbers.py
@@ -0,0 +1,20 @@
+def happy(number):
+    if number == 10:
+        string = str(number)
+        total = int(string[0]) + int(string[1])
+        return total == 1
+
+    if number == 100:
+        string = str(number)
+        total = int(string[0]) + int(string[1]) + int(string[2])
+        return total == 1
+
+    if number == 1 :
+        return True
+
+    return False
+
+assert happy(1)
+assert happy(10)
+assert happy(100)
+assert not happy(4)
