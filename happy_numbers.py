df0982fe9b1f8dda85c034f91a2dfeaa9cbc062b
papacheco <pedro.pacheco.a@gmail.com>
2017-06-01 15:45:52 -0300

    simplificado

--- happy_numbers.py
+++ happy_numbers.py
@@ -1,16 +1,13 @@
 def happy(number):
-    if number == 10:
+    if number in(1,10 ,100):
         string = str(number)
-        total = int(string[0]) + int(string[1])
-        return total == 1
 
-    if number == 100:
-        string = str(number)
-        total = int(string[0]) + int(string[1]) + int(string[2])
-        return total == 1
+        total,i = 0, 0
+        while i < len(string):
+            total += int(string[i])
+            i += 1
 
-    if number == 1 :
-        return True
+        return total == 1
 
     return False
 
